e307d8b3d07c7aef84f0d1dd24d1174a56edb822
djura.smits <djura.smits@608951c4-64cf-b991-5086-fee498d22fdf>
2014-10-17 12:31:41 +0000

    - Generated 2 graphs with goal behavior included - Modified situations.xml so there is only one big slowwander and one big fastwander situation area instead of many little areas. - Modified the movement area in AbstractBaseMasonAction but don't know if it has any effect.

--- trunk/hulpscriptjes/plotactions.m
+++ trunk/hulpscriptjes/plotactions.m
@@ -1,9 +1,9 @@
 results = importdata('/home/djura/vakken/afstudeerstage/hulpscriptjes/results.csv');
 %%
 plot(results(1:200, :));
-legend('  GoToToiletMasonAction ',  '  StandStillMasonAction ',  '  SlowWanderMasonAction ',  '  FastWanderMasonAction ',  '  WaitForFriendMasonAction ',  '  IdleMasonAction ');
+legend('  StandStillMasonAction ',  '  FastWanderMasonAction ',  '  GoToGoalMasonAction ',  '  SlowWanderMasonAction ',  '  IdleMasonAction ');
 
 
-title('Plot using Gaussian probability function with mean=200 sigma=50.')
+title('Plot using Linear utility function with utility=0 at 0 and utility=1 at 200. Goal utility function is always 0.1.')
 %%TODO: Plot only from 1 to 200 steps or even  less
 
