84c08c4e76e48d1c5af88255add185e97413f10e
djura.smits <djura.smits@608951c4-64cf-b991-5086-fee498d22fdf>
2014-10-22 09:01:41 +0000

    - Moahr graphs

--- trunk/hulpscriptjes/plotactions.m
+++ trunk/hulpscriptjes/plotactions.m
@@ -1,9 +1,9 @@
 results = importdata('/home/djura/vakken/afstudeerstage/hulpscriptjes/results.csv');
 %%
 plot(results(1:200, :));
-legend('  GoToToiletMasonAction ',  '  SlowWanderMasonAction ',  '  FastWanderMasonAction ',  '  GoToGoalMasonAction ',  '  WaitForFriendMasonAction ',  '  IdleMasonAction ');
+legend('  StandStillMasonAction ',  '  IdleMasonAction ',  '  SlowWanderMasonAction ');
 
 
-title('Plot using Sigmoid utility function shifted 100 and stretched times 10 on the x axis. Goal utility function 1 at t=0 and 0 at t=200.')
+title('Plot Sigmoid utility function shifted 100, stretched times 10 on x axis, multiplied by -1 on y axis. Goal utility function Sigmoid shifted 100, stretched times 10 on x axis, multiplied by -1 on y axis.')
 %%TODO: Plot only from 1 to 200 steps or even  less
 
