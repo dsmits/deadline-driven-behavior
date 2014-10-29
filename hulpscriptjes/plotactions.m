60ca4c7907ccf970774182d024c3678960abe906
djura.smits <djura.smits@608951c4-64cf-b991-5086-fee498d22fdf>
2014-10-29 15:37:01 +0000

    - Added new graphs to thesis - Written a little about first results

--- hulpscriptjes/plotactions.m
+++ hulpscriptjes/plotactions.m
@@ -1,9 +1,9 @@
 results = importdata('/home/djura/vakken/afstudeerstage/hulpscriptjes/results.csv');
 %%
 plot(results(1:200, :));
-legend('  StandStillMasonAction ',  '  IdleMasonAction ',  '  SlowWanderMasonAction ');
+legend('  StandStillMasonAction ',  '  FastWanderMasonAction ',  '  GoToGoalMasonAction ',  '  SlowWanderMasonAction ',  '  IdleMasonAction ');
 
 
-title('Plot Sigmoid utility function shifted 100, stretched times 10 on x axis, multiplied by -1 on y axis. Goal utility function Sigmoid shifted 100, stretched times 10 on x axis, multiplied by -1 on y axis.')
+title('Plot with utility function Gaussian(200, 50, 1), and goal utility function Gaussian(0,50,1)');
 %%TODO: Plot only from 1 to 200 steps or even  less
 
