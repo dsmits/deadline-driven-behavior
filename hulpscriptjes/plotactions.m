4671a79861f0c90c41fa21a2aea5573eb475598e
djura.smits <djura.smits@608951c4-64cf-b991-5086-fee498d22fdf>
2014-11-05 12:15:01 +0000

    - Modified results a bit

--- hulpscriptjes/plotactions.m
+++ hulpscriptjes/plotactions.m
@@ -1,9 +1,15 @@
 results = importdata('/home/djura/vakken/afstudeerstage/hulpscriptjes/results.csv');
 %%
 plot(results(1:200, :));
-legend('  StandStillMasonAction ',  '  FastWanderMasonAction ',  '  GoToGoalMasonAction ',  '  SlowWanderMasonAction ',  '  IdleMasonAction ');
+
+[hleg1, hobj1] = legend('StandStillMasonAction',	'FastWanderMasonAction',	'GoToGoalMasonAction',	'SlowWanderMasonAction',	'IdleMasonAction');
+legenddimensions = get(hleg1, 'position');
+%legendimensions(3) = legenddimensions(3)*10
+%legendimensions(4) = legenddimensions(4)*10
+%set(hleg1,'position', legenddimensions)
+%set(hleg1,'position',[x0 y0 width height])
 
 
-title('Plot with utility function Gaussian(200, 50, 1), and goal utility function Gaussian(0,50,1)');
+title('Plot with linear utility function from 0 to 1, and goal utility function Sigmoid(100,0,-10,1)');
 %%TODO: Plot only from 1 to 200 steps or even  less
 
--- hulpscriptjes/sigmoidplot.m
+++ hulpscriptjes/sigmoidplot.m
@@ -12,4 +12,16 @@ y = ystretch ./ numerator -yshift;
 plot(y);
 plot(t,y)
 
-%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+t = -6:0.01:6;
+xshift  = 0;
+yshift = 0;
+xstretch = 1;
+ystretch = 1;
+exponent = -1*((t-xshift)/xstretch);
+numerator = 1 + exp(exponent);
+ystretch ./ numerator - yshift;
+y = ystretch ./ numerator -yshift;
+plot(y);
+plot(t,y)
